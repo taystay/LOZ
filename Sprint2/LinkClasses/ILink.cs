14f8b151fe0acbb5c17b075b2757ed110dc6cdf7
Taylor Liu <liu.7891@buckeyemail.osu.edu>
2022-02-02 14:59:51 -0500

    Added .gitignore

---
+++ Sprint2/LinkClasses/ILink.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Sprint2.LinkClasses
+{
+
+    public interface ILink
+    {
+        public void Update();
+    }
+}
---
+++ Sprint2/LinkClasses/LinkClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Sprint2.LinkClasses
+{
+    class LinkIdle : ILink
+    {
+        //-----Private Variables-----
+        private Rectangle Frame;
+        private Texture2D Texture;
+        private double Scale;
+
+        public void Update()
+        {
+
+        }
+
+        public void Attack()
+        {
+
+        }
+
+        public void Move()
+        {
+
+        }
+    }
+}
