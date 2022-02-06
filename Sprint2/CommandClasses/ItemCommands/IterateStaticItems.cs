9392a1372f8c4aa80bf48e992367deca2fe87ab9
Keit Vu <keit.vu@gmail.com>
2022-02-06 17:57:47 -0500

    Merge

---
+++ Sprint2/CommandClasses/ItemCommands/IterateStaticItems.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Sprint2
+{
+    class IterateStaticItems :ICommand
+    {
+        private Game1 gameObject;
+        public IterateStaticItems(Game1 obj)
+        {
+            gameObject = obj;
+        }
+        public void execute()
+        {
+            gameObject.Item++;   
+        }
+    }
+}
---
+++ Sprint2/ControllerClasses/MouseController/MouseController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sprint2
+{
+    class MouseController : IController
+    {
+        private Dictionary<Rectangle,ICommand> LeftHitLocations;
+        private List<ICommand> RightClickCommands;
+
+        public MouseController()
+        {
+            LeftHitLocations = new Dictionary<Rectangle, ICommand>();
+            RightClickCommands = new List<ICommand>();
+        }
+
+        public void RegisterRightClickCommands(ICommand command)
+        {
+            RightClickCommands.Add(command);
+        }
+
+        public void RegisterLeftClickLocation(Rectangle location, ICommand command)
+        {
+            LeftHitLocations.Add(location, command);
+        }
+
+        public void Update(GameTime gametime)
+        {
+            MouseState State = Mouse.GetState();
+            if(State.RightButton == ButtonState.Pressed)
+            {
+                foreach(ICommand command in RightClickCommands)
+                {
+                    command.execute();
+                }
+            }
+
+            if(State.LeftButton == ButtonState.Pressed)
+            {
+                foreach(KeyValuePair<Rectangle, ICommand> pair in LeftHitLocations)
+                {
+                    if(State.X >= pair.Key.X && 
+                        State.Y >= pair.Key.Y && 
+                        State.X <= pair.Key.X + pair.Key.Width && 
+                        State.Y <= pair.Key.Y + pair.Key.Height)
+                    {
+                        pair.Value.execute();
+                    }
+                }
+            }
+        }
+    }
+}
