cc9f75733adcd022de2dd960b050b4967218e519
Tonyy456 <ajdalesandro0115@gmail.com>
2022-02-10 17:20:25 -0500

    Added a singleton for game objects! Very nice very nice. Put items in there, later tonight ill add the blocks enemies and more into there.

--- Sprint2/Game1.cs
+++ Sprint2/Game1.cs
@@ -15,13 +15,10 @@ namespace Sprint2
         private Point screenDimensions;
         private List<IController> controllerList;
         
-        //----------------------
+        //-----------------------
         private List<IItem> linkItems;
-        private List<IItem> items;
         private List<IEnvironment> blocks;
-        public int Item { get; set; }
         public int Block { get; set; }
-        public Point ItemLocation { get; set; } = new Point(500, 500);
         private IEnemy enemy;
         private ILink link;
 
@@ -75,21 +72,8 @@ namespace Sprint2
                 { new SolidBlueBlock(new Point(400, 400), scale) },
                 { new StairsBlock(new Point(400, 400), scale) },
             };
-            items = new List<IItem>()
-            {
-                { new ArrowItem(ItemLocation, scale) },
-                { new Bow(ItemLocation, scale) },
-                { new Clock(ItemLocation, scale) },
-                { new Compass(ItemLocation, scale) },
-                { new Fairy(ItemLocation, scale) },
-                { new FireItem(ItemLocation, scale) },
-                { new Heart(ItemLocation, scale) },
-                { new HeartContainer(ItemLocation, scale) },
-                { new Key(ItemLocation, scale) },
-                { new Map(ItemLocation, scale) },
-                { new Rupee(ItemLocation, scale) },
-                { new Triforce(ItemLocation, scale) },
-            };
+            GameObjects.Instance.AddSprint2Items();
+            
             link = new Link(new Point(200, 200));       
             enemy = new Dragon(new Point(800, 800));
 
@@ -102,11 +86,7 @@ namespace Sprint2
                 controller.Update(gameTime);
             }
 
-            if (Item >= items.Count)
-                Item = 0;
-            if (Item < 0)
-                Item = items.Count - 1;
-            items[Item].Update(gameTime);
+            
 
             // Allows for items to remove themselves.
             int i = 0;
@@ -122,11 +102,7 @@ namespace Sprint2
                 i++;
             }
 
-            if (Block >= blocks.Count)
-                Block = 0;
-            if (Block < 0)
-                Block = blocks.Count - 1;
-            blocks[Block].Update(gameTime);
+            GameObjects.Instance.UpdateItems(gameTime);
 
             //--------------------------------------------------
             link.Update(gameTime);
@@ -141,7 +117,7 @@ namespace Sprint2
 
             link.Draw(spriteBatch);
             enemy.Draw(spriteBatch);
-            items[Item].Draw(spriteBatch);
+            GameObjects.Instance.DrawItems(spriteBatch);
             blocks[Block].Draw(spriteBatch);
 
             foreach (IItem item in linkItems)
---
+++ Sprint2/GameObjects.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Sprint2
+{
+    class GameObjects
+    {
+        //----------ITEMS
+		private static List<IItem> items;
+		private static GameObjects instance = new GameObjects();
+        private static int itemIndex = 0;
+
+        //-----------LINK
+        
+        public static int ItemIndex
+        {
+            get
+            {
+                return itemIndex;
+            }
+            set
+            {
+                itemIndex = value;
+            }
+        }
+
+        private Point itemLocations = new Point(500, 500);
+
+		public static GameObjects Instance
+		{
+			get
+			{
+				return instance;
+			}
+		}
+
+		private GameObjects()
+		{
+			items = new List<IItem>();
+		}
+
+		public void SpawnItem(IItem item)
+        {
+			items.Add(item);
+        }
+
+		public void AddSprint2Items()
+        {
+			double scale = 3.0;
+            items = new List<IItem>()
+            {
+                { new ArrowItem(itemLocations, scale) },
+                { new Bow(itemLocations, scale) },
+                { new Clock(itemLocations, scale) },
+                { new Compass(itemLocations, scale) },
+                { new Fairy(itemLocations, scale) },
+                { new FireItem(itemLocations, scale) },
+                { new Heart(itemLocations, scale) },
+                { new HeartContainer(itemLocations, scale) },
+                { new Key(itemLocations, scale) },
+                { new Map(itemLocations, scale) },
+                { new Rupee(itemLocations, scale) },
+                { new Triforce(itemLocations, scale) },
+            };
+        }
+
+		public void UpdateItems(GameTime gameTime)
+        {
+            if (itemIndex >= items.Count)
+                itemIndex = 0;
+            if (itemIndex < 0)
+                itemIndex = items.Count - 1;
+            items[itemIndex].Update(gameTime);
+        }
+
+		public void DrawItems(SpriteBatch spriteBatch)
+        {
+            items[itemIndex].Draw(spriteBatch);
+        }
+	}
+}
