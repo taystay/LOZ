9a4aaab7c290a454c56cbba04c2fd60ba83e031d
Taylor Liu <liu.7891@buckeyemail.osu.edu>
2022-02-20 22:18:35 -0500

    Final commit of Sprint 2

--- Sprint2/ControllerClasses/KeyboardController.cs
+++ Sprint2/ControllerClasses/KeyboardController.cs
@@ -8,6 +8,7 @@ namespace Sprint2.ControllerClasses
     class KeyboardController : IController
     {
         private Dictionary<Keys, ICommand> storedInitCommands;
+        private Dictionary<Keys, int> initKeysTimeHeld;
         private Dictionary<Keys, ICommand> storedHoldCommands;
         private Dictionary<Keys, ICommand> storedReleaseCommands;
         private List<Keys> initKeysPressed;
@@ -20,6 +21,7 @@ namespace Sprint2.ControllerClasses
             storedReleaseCommands = new Dictionary<Keys, ICommand>();
             initKeysPressed = new List<Keys>();
             releaseKeysPressed = new List<Keys>();
+            initKeysTimeHeld = new Dictionary<Keys, int>();
         }
 
         public void RegisterInitialCommand(Keys key, ICommand initialPressCommand)
@@ -44,12 +46,24 @@ namespace Sprint2.ControllerClasses
                 if (initKeysPressed.Contains(key)) continue;
                 if (!storedInitCommands.ContainsKey(key)) continue;
                 initKeysPressed.Add(key);
+                if (!initKeysTimeHeld.ContainsKey(key))
+                    initKeysTimeHeld.Add(key, 20);
                 storedInitCommands[key].execute();
             }
             i = 0;
             while (i < initKeysPressed.Count)
             {
                 Keys key = initKeysPressed[i++];
+                if (initKeysTimeHeld[key] == 0)
+                {
+                    initKeysTimeHeld.Remove(key);
+                    initKeysPressed.Remove(key);
+                    continue;
+                }
+                else
+                {
+                    initKeysTimeHeld[key]--;
+                }
                 if (Keyboard.GetState().IsKeyDown(key)) continue;
                 initKeysPressed.RemoveAt(--i);
             }
