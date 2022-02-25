808358e6867480a6df69b7b33a8d237a200eafaf
Taylor Liu <liu.7891@buckeyemail.osu.edu>
2022-02-25 17:19:42 -0500

    Added notes for how to implement the collision stuff

--- LOZ/Collision/CollisionBehavior.cs
+++ LOZ/Collision/CollisionBehavior.cs
@@ -13,9 +13,15 @@ namespace LOZ.Collision.Iterator
         public void HandleCollision(IGameObjects one, IGameObjects two, CollisionSide side)
         {
             /*
+             * IDEA 1: Use enums and another class to tell us the type of collision and another class to handle the collison behavior
              * type = CollisionType(one, two);
              * Collide(type, side);
              * 
+             * (IMPLEMENT THIS ONE) IDEA 2: Use if else to determine which type of collision and then call another class and method
+             * IF STATEMENTS
+             * if(one.InstanceOf(new ILink) && two.InstanceOf(new IEnvironemnt)) {
+             *      LinkEnvironmentCollisionHandler(one, two, side);
+             * } else if (So on and so forth) {}
              */
         }
     }
