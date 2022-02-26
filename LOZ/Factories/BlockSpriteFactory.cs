3f1fefcb664b7f171021754be9a85c8a3bbe7efc
Taylor Liu <liu.7891@buckeyemail.osu.edu>
2022-02-26 15:43:31 -0500

    Fixed block and item factories to include the folder SpriteClasses so they can use ISprite

--- LOZ/Factories/BlockSpriteFactory.cs
+++ LOZ/Factories/BlockSpriteFactory.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using LOZ.SpriteClasses.BlockSprites;
+using LOZ.SpriteClasses;
 
 namespace LOZ.Factories
 {
--- LOZ/Factories/ItemFactory.cs
+++ LOZ/Factories/ItemFactory.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using LOZ.SpriteClasses.ItemSprites;
+using LOZ.SpriteClasses;
 
 namespace LOZ.Factories
 {
