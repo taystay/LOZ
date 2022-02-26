2449a01a0db90f7537743a639d57aaa458572bde
Keit Vu <keit.vu@gmail.com>
2022-02-26 18:15:25 -0500

    Started Collision detection wrote some ideas about collision direction.

--- LOZ/Collision/CollisionDetection.cs
+++ LOZ/Collision/CollisionDetection.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using Microsoft.Xna.Framework;
+
 namespace LOZ.Collision
 {
     enum CollisionSide
@@ -8,20 +9,32 @@ namespace LOZ.Collision
         Left,
         Right
     }
-    //private bool collides;
-    //private CollisionSide side; 
     class CollisionDetection
     { 
         public CollisionDetection()
         {
-
         }
         public void CheckCollision(IGameObjects objOne, IGameObjects objTwo)
         {
-            /*
-            if(collides)
-                CollsionBehavior(objOne, objTwo, side)
-            */
+            Rectangle rec1 = objOne.GetHitBox();
+            Rectangle rec2 = objTwo.GetHitBox();
+
+            if (rec1.Intersects(rec2)) {
+                //rec1 is the focal point 
+
+                //if positive difference in X: rec2 is left side(collision = left)
+                //if negative difference in X: rec2 is right side (collision =right)
+                //if positive difference in Y: rec2 is top (collision =top)
+                //if negative difference in Y: rec2 is bottom (collision = bottom) 
+                int differenceInX = rec1.X - rec2.X;
+                int differenceInY = rec1.Y - rec2.Y;
+                Rectangle intersectRect = Rectangle.Intersect(rec1, rec2);
+                
+            
+            
+            }
         }
+
+
     }
 }
