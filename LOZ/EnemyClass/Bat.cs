d3d9794eadc12b74b7102185db74bafaa45878c8
Tonyy456 <ajdalesandro0115@gmail.com>
2022-03-08 12:41:44 -0500

    Added a more interesting behavior for how bats attack you. They come at your 3.5 pixels per update  but have a chance to random swerve into another direction. It makes it less like they are sitting sucks.

--- LOZ/EnemyClass/Bat.cs
+++ LOZ/EnemyClass/Bat.cs
@@ -2,16 +2,20 @@
 using Microsoft.Xna.Framework;
 using LOZ.Factories;
 using LOZ.Collision;
+using LOZ.GameState;
 
 namespace LOZ.EnemyClass
 {
     class Bat : AbstractEnemy
     {
+        private double vMag = 3.5;
+        private protected Vector2 velocity2;
         public Bat(Point location)
         {
             Position = location;
             _texture = EnemySpriteFactory.Instance.CreateBat();
             random = new Random();
+            velocity2 = new Vector2(0, 0);
 
         }
 
@@ -24,8 +28,29 @@ namespace LOZ.EnemyClass
         {
             if ((int)timer.TotalGameTime.TotalMilliseconds % 1000 == 0)
             {
-                velocity.X = random.Next(-2, 2);
-                velocity.Y = random.Next(-2, 2);
+                Point linkP = CurrentRoom.Instance.Room.Link.Position;
+
+                //https://stackoverflow.com/questions/41317291/setting-the-magnitude-of-a-2d-vector#41321162
+                if (random.Next(2) % 2 == 0)
+                {
+                    double dx = (linkP.X - Position.X);
+                    double dy = (linkP.Y - Position.Y);
+
+                    double mag = Math.Sqrt(dx * dx + dy * dy);
+
+                    
+
+                    dx = dx * vMag / mag;
+                    dy = dy * vMag / mag;
+                    velocity2.X = (float)dx;
+                    velocity2.Y = (float)dy;
+                } else
+                {
+                    double dx = (int)random.Next(-2,2);
+                    double dy = (int)random.Next(-2,2);
+                    velocity2.X = (float)dx;
+                    velocity2.Y = (float)dy;
+                }
             }
 
             if(IsDamaged)
@@ -35,7 +60,7 @@ namespace LOZ.EnemyClass
                     IsDamaged = false;
             }
 
-            modifyPosition(velocity.X, velocity.Y);
+            modifyPosition((int)velocity2.X, (int)velocity2.Y);
 
             _texture.Update(timer);
         }
