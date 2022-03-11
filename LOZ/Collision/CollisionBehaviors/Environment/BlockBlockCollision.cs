461f42a3280b161e68e9c219710d7b35d4c547a4
Taylor Liu <liu.7891@buckeyemail.osu.edu>
2022-03-11 17:38:05 -0500

    Removed some duplicate code and made it so the pushable block no longer blocks the entrance of the basement

--- LOZ/Collision/CollisionBehaviors/Environment/BlockBlockCollision.cs
+++ LOZ/Collision/CollisionBehaviors/Environment/BlockBlockCollision.cs
@@ -10,37 +10,35 @@ namespace LOZ.Collision
         public static void Handle(IGameObjects blockObj1, IGameObjects blockObj2, CollisionSide side)
         {
             IEnvironment block1 = (IEnvironment)blockObj1;
+            IEnvironment block2 = (IEnvironment)blockObj2;
             Point blockPos1 = block1.Position;
             Rectangle linkBox = block1.GetHitBox().ToRectangle();
             Rectangle collisionBox = Rectangle.Intersect(linkBox, blockObj2.GetHitBox().ToRectangle());
+            Point linkPos = Room.Link.Position;
 
             if (!block1.Pushable) return;
 
-            if (side == CollisionSide.Top)
+            if (side == CollisionSide.Top && !TypeC.Check(block2, typeof(StairsBlock)))
             {
                 block1.Position = new Point(blockPos1.X, blockPos1.Y - collisionBox.Height);
-                Point linkPos = Room.Link.Position;
                 Room.Link.Position = new Point(linkPos.X, linkPos.Y - collisionBox.Height);
             }
                 
-            else if (side == CollisionSide.Left)
+            else if (side == CollisionSide.Left && !TypeC.Check(block2, typeof(StairsBlock)))
             {
                 block1.Position = new Point(blockPos1.X - collisionBox.Width, blockPos1.Y);
-                Point linkPos = Room.Link.Position;
                 Room.Link.Position = new Point(linkPos.X - collisionBox.Width, linkPos.Y);
             }
                 
-            else if (side == CollisionSide.Right)
+            else if (side == CollisionSide.Right && !TypeC.Check(block2, typeof(StairsBlock)))
             {
                 block1.Position = new Point(blockPos1.X + collisionBox.Width, blockPos1.Y);
-                Point linkPos = Room.Link.Position;
                 Room.Link.Position = new Point(linkPos.X + collisionBox.Width, linkPos.Y);
             }
                 
-            else if (side == CollisionSide.Bottom)
+            else if (side == CollisionSide.Bottom && !TypeC.Check(block2, typeof(StairsBlock)))
             {
                 block1.Position = new Point(blockPos1.X, blockPos1.Y + collisionBox.Height);
-                Point linkPos = Room.Link.Position;
                 Room.Link.Position = new Point(linkPos.X, linkPos.Y + collisionBox.Height);
             }
                 
