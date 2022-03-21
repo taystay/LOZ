751b4c67f8a8f40e4138d6768bf43bdeda3f8b9f
Tonyy456 <ajdalesandro0115@gmail.com>
2022-03-21 13:16:21 -0400

    Added framework classes.

---
+++ LOZ/src/Misc/Hud/UserCurrentItemHud.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LOZ.src.Misc.Hud
+{
+    class UserCurrentItemHud : HudElement
+    {
+        public UserCurrentItemHud()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
