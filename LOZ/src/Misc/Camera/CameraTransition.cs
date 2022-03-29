0c8627659956104188cbf703f4935e7d2ae12932
Keit Vu <keitvu@gmail.com>
2022-03-29 00:19:08 -0400

    Began Camera transition file
    Began creating the camera transition

---
+++ LOZ/src/Misc/Camera/CameraTransition.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace LOZ.Camera
+{
+    class CameraTransition
+    {
+        SpriteBatch spriteBatch;
+
+        public CameraTransition()
+        {
+        }
+
+        //public void DrawLeftRoom(Point3D location) {
+        //    Matrix.CreateTranslation(location);
+        //}
+        public void DrawRightRoom() { }
+        public void DrawUpRoom() { }
+        public void DrawDownRoom() { }
+
+
+    }
+}
