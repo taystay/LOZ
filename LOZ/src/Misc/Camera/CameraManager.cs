2d8f7c63e421f24e9669c5e42250fcab2c0c4049
Keit Vu <keitvu@gmail.com>
2022-03-30 01:27:30 -0400

    Camera In-progress
    Began to formulate how to do camera translation. I have a grasp on how to do the camera translation now

---
+++ LOZ/src/Misc/Camera/CameraManager.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace LOZ.Camera
+{
+    class CameraManager
+    {
+        SpriteBatch spriteBatch;
+        Matrix translation;
+        Texture2D roomLayout;
+
+        public CameraManager(ContentManager content)
+        {
+            translation = Matrix.CreateTranslation(0, 0, 0);
+            roomLayout = content.Load<Texture2D>("dungeonRooms");
+            translation = Matrix.CreateScale(2f);
+        }
+
+        public void TranslationToRoom(int x, int y) {
+
+
+
+        }
+        //what to do
+        //using dungeons rooms I will create the scale
+        //then in draw it will constantly update pixel by pixel for room translation
+        //transition from the dungeonRoom png to show
+        //then all the item spawn
+        
+
+
+    }
+}
