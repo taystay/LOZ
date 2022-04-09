2eeb7a3dc7c18c3cae9ee627f81d442cb4cd0308
Keit Vu <keitvu@gmail.com>
2022-04-08 23:56:12 -0400

    Began Room Refactor
    began to create individual rooms. Will need to change the parser for the new rooms. Created a interface, abstract room, and just the start room with thought in the start room

---
+++ LOZ/src/Room/AllRooms/IRoom.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+namespace LOZ.Room
+{
+    public interface IRoom 
+    {
+        public void Update(GameTime gameTime);
+        public void Draw(SpriteBatch spriteBatch);
+        public void RemoveItems();
+    }
+}
