9539534268df802aa1410acb4bcbcf70bab1e0bc
Jose Berrios <berrios.24@buckeyemail.osu.edu>
2022-04-22 01:40:00 -0400

    ExtraBombs, GetPortalGun(unfinished), UnlimitedArrows, Updated KeyBindings, KeyboardController

---
+++ LOZ/src/Input/CommandClasses/Cheats/UnlimitedArrows.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LOZ.LinkClasses;
+using LOZ.GameStateReference;
+using LOZ.Inventory;
+using LOZ.ItemsClasses;
+using LOZ.Sound;
+
+namespace LOZ.CommandClasses
+{
+    class UnlimitedArrows : ICommand
+    {
+        private Game1 game;
+        public UnlimitedArrows(Game1 game) => this.game = game;
+         
+        public void execute()
+        {
+            //when bombs reach 0 will not work when adding bombs
+            LinkInventory inv = RoomReference.GetInventory();
+            inv.rupeeCount = 999;
+            SoundManager.Instance.SoundToPlayInstance(SoundEnum.Secret);
+        }
+    }
+}
